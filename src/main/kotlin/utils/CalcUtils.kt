b0dabb824d7dcd4d9086c6a662f950c8202430e7
Thomasaja <95894629+Thomasaja@users.noreply.github.com>
2021-12-19 23:34:29 +0700

    Initial commit

---
+++ src/main/kotlin/utils/CalcUtils.kt
@@ -0,0 +1,19 @@
+package utils
+
+class CalcUtils {
+    fun plus(a: Int, b : Int) : Int{
+        return a + b
+
+    }
+    fun minus(a: Int, b: Int) : Int{
+        return a - b
+    }
+    fun times(a: Int, b: Int) : Int{
+        return a * b
+    }
+    fun divided(a: Int, b: Int) : Int{
+        return a / b
+    }
+
+
+}
